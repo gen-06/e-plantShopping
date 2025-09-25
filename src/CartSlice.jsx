9bb43a44ece3cac6c7787c893e652b8921e65bc1
gen-06 <genalpha089@gmail.com>
2025-09-25 11:50:48 -0400

    state_management_using_redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cos} = action.payload; //destructure product details from the action payload
+        // check if the item already exist in the cart by comparing names
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            // if item already exists in the cart, increase its quantity
+            existingItem.quantityquantity++;
+        } else {
+            // if item dost not exist, add it to the cart with quantity 1
+            state.items.push({name, image, cost, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const {name, quantity } = action.payload; // destructure the product name and new quantity from the action
+        // find the item in the cart that matches the given name
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity; // if the item is found , update its quantity to the new value
+        }
     },
   },
 });
