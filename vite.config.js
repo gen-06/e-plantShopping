0c2f9b82a43b173d1b25b6872fe12d7731096518
gen-06 <genalpha089@gmail.com>
2025-09-26 13:56:13 -0400

    url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "https://github.com/gen-06/e-plantShopping.git",
   plugins: [react()],
 })
